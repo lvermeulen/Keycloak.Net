b2e1a4a4b0037ec1a2432b0c6748835e330da5f5
Luk Vermeulen <luk.vermeulen@gmail.com>
2019-07-05 09:25:54 +0200

    add /clients

---
+++ src/Keycloak.Net/Models/Clients/Client.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json;
+
+namespace Keycloak.Net.Models.Clients
+{
+
+    public class Client
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("clientId")]
+        public string ClientId { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("baseUrl")]
+        public string BaseUrl { get; set; }
+        [JsonProperty("surrogateAuthRequired")]
+        public bool SurrogateAuthRequired { get; set; }
+        [JsonProperty("enabled")]
+        public bool Enabled { get; set; }
+        [JsonProperty("clientAuthenticatorType")]
+        public string ClientAuthenticatorType { get; set; }
+        [JsonProperty("redirectUris")]
+        public string[] RedirectUris { get; set; }
+        [JsonProperty("webOrigins")]
+        public object[] WebOrigins { get; set; }
+        [JsonProperty("notBefore")]
+        public int NotBefore { get; set; }
+        [JsonProperty("bearerOnly")]
+        public bool BearerOnly { get; set; }
+        [JsonProperty("consentRequired")]
+        public bool ConsentRequired { get; set; }
+        [JsonProperty("standardFlowEnabled")]
+        public bool StandardFlowEnabled { get; set; }
+        [JsonProperty("implicitFlowEnabled")]
+        public bool ImplicitFlowEnabled { get; set; }
+        [JsonProperty("directAccessGrantsEnabled")]
+        public bool DirectAccessGrantsEnabled { get; set; }
+        [JsonProperty("serviceAccountsEnabled")]
+        public bool ServiceAccountsEnabled { get; set; }
+        [JsonProperty("publicClient")]
+        public bool PublicClient { get; set; }
+        [JsonProperty("frontchannelLogout")]
+        public bool FrontChannelLogout { get; set; }
+        [JsonProperty("protocol")]
+        public string Protocol { get; set; }
+        [JsonProperty("attributes")]
+        public ClientAttributes Attributes { get; set; }
+        [JsonProperty("authenticationFlowBindingOverrides")]
+        public AuthenticationFlowBindingOverrides AuthenticationFlowBindingOverrides { get; set; }
+        [JsonProperty("fullScopeAllowed")]
+        public bool FullScopeAllowed { get; set; }
+        [JsonProperty("nodeReRegistrationTimeout")]
+        public int NodeReregistrationTimeout { get; set; }
+        [JsonProperty("protocolMappers")]
+        public ClientProtocolmapper[] ProtocolMappers { get; set; }
+        [JsonProperty("defaultClientScopes")]
+        public string[] DefaultClientScopes { get; set; }
+        [JsonProperty("optionalClientScopes")]
+        public string[] OptionalClientScopes { get; set; }
+        [JsonProperty("access")]
+        public ClientAccess Access { get; set; }
+    }
+
+    public class ClientAttributes
+    { }
+
+    public class AuthenticationFlowBindingOverrides
+    { }
+
+    public class ClientAccess
+    {
+        [JsonProperty("view")]
+        public bool View { get; set; }
+        [JsonProperty("configure")]
+        public bool Configure { get; set; }
+        [JsonProperty("manage")]
+        public bool Manage { get; set; }
+    }
+
+    public class ClientProtocolmapper
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("protocol")]
+        public string Protocol { get; set; }
+        [JsonProperty("protocolMapper")]
+        public string ProtocolMapper { get; set; }
+        [JsonProperty("consentRequired")]
+        public bool ConsentRequired { get; set; }
+        [JsonProperty("config")]
+        public ClientConfig Config { get; set; }
+    }
+
+    public class ClientConfig
+    {
+        [JsonProperty("userinfotokenclaim")]
+        public string UserInfoTokenClaim { get; set; }
+        [JsonProperty("userattribute")]
+        public string UserAttribute { get; set; }
+        [JsonProperty("idtokenclaim")]
+        public string IdTokenClaim { get; set; }
+        [JsonProperty("accesstokenclaim")]
+        public string AccessTokenClaim { get; set; }
+        [JsonProperty("claimname")]
+        public string ClaimName { get; set; }
+        [JsonProperty("jsonTypelabel")]
+        public string JsonTypelabel { get; set; }
+    }
+}
---
+++ test/Keycloak.Net.Tests/Clients/KeycloakClientShould.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Keycloak.Net.Tests
+{
+    public partial class KeycloakClientShould
+    {
+        [Theory]
+        [InlineData("Insurance")]
+        public async Task GetClientsAsync(string realm)
+        {
+            var result = await _client.GetClientsAsync(realm);
+            
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+        }
+    }
+}
