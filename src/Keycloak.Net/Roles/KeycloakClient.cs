c6331edb1c364d27b99fc6d38f37976d35bf0e58
Luk Vermeulen <luk.vermeulen@gmail.com>
2019-07-10 17:01:56 +0200

    fix appveyor.yml

---
+++ src/Keycloak.Net/Roles/KeycloakClient.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Flurl.Http;
+using Keycloak.Net.Models.Key;
+
+namespace Keycloak.Net
+{
+    public partial class KeycloakClient
+    {
+        public async Task<KeysMetadataRepresentation> GetRolesAsync(string realm) => await GetBaseUrl(realm)
+            .AppendPathSegment($"/admin/realms/{realm}/keys")
+            .GetJsonAsync<KeysMetadataRepresentation>()
+            .ConfigureAwait(false);
+    }
+}
---
+++ test/Keycloak.Net.Tests/Roles/KeycloakClientShould.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Keycloak.Net.Tests
+{
+    public partial class KeycloakClientShould
+    {
+        [Theory]
+        [InlineData("Insurance")]
+        public async Task GetRolesAsync(string realm)
+        {
+            var result = await _client.GetRolesAsync(realm);
+            Assert.NotNull(result);
+        }
+    }
+}
