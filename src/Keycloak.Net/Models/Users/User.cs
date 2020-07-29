32c26b9430f1d7085aa58eb1c8b9cd05e9213c2f
Luk Vermeulen <luk.vermeulen@gmail.com>
2020-07-29 12:42:37 +0200

    add missing properties to User

--- src/Keycloak.Net/Models/Users/User.cs
+++ src/Keycloak.Net/Models/Users/User.cs
@@ -34,5 +34,25 @@ namespace Keycloak.Net.Models.Users
         public UserAccess Access { get; set; }
         [JsonProperty("attributes")]
         public Dictionary<string, IEnumerable<string>> Attributes { get; set; }
+        [JsonProperty("clientConsents")]
+        public IEnumerable<UserConsent> ClientConsents { get; set; }
+        [JsonProperty("clientRoles")]
+        public IDictionary<string, object> ClientRoles { get; set; }
+        [JsonProperty("credentials")]
+        public IEnumerable<Credentials> Credentials { get; set; }
+        [JsonProperty("federatedIdentities")]
+        public IEnumerable<FederatedIdentity> FederatedIdentities { get; set; }
+        [JsonProperty("federationLink")]
+        public string FederationLink { get; set; }
+        [JsonProperty("groups")]
+        public IEnumerable<string> Groups { get; set; }
+        [JsonProperty("origin")]
+        public string Origin { get; set; }
+        [JsonProperty("realmRoles")]
+        public IEnumerable<string> RealmRoles { get; set; }
+        [JsonProperty("self")]
+        public string Self { get; set; }
+        [JsonProperty("serviceAccountClientId")]
+        public string ServiceAccountClientId { get; set; }
     }
 }
