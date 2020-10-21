444baf2117dfc664380aa3a167b493a2df01da81
Slavko Salontaji <s.salontaji@vegaitsourcing.rs>
2020-10-21 11:58:53 +0200

    (Authorization)Resources support

---
+++ src/Keycloak.Net/Models/AuthorizationResources/AuthorizationResource.cs
@@ -0,0 +1,26 @@
+﻿using Keycloak.Net.Models.AuthorizationScopes;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Keycloak.Net.Models.AuthorizationResources
+{
+    public class AuthorizationResource
+    {
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+        [JsonProperty("scopes")]
+        public IEnumerable<AuthorizationScope> Scopes { get; set; }
+        [JsonProperty("attributes")]
+        public Dictionary<string, IEnumerable<string>> Attributes { get; set; }
+        [JsonProperty("uris")]
+        public IEnumerable<string> Uris { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("ownerManagedAccess")]
+        public bool? OwnerManagedAccess { get; set; }
+        [JsonProperty("displayName")]
+        public string DisplayName { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; } 
+    }
+}
