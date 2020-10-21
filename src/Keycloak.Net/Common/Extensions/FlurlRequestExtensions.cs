8d6c64de1d136dd2255d47e7631baf3b19559232
Slavko Salontaji <s.salontaji@vegaitsourcing.rs>
2020-10-21 13:53:20 +0200

    Forwarded HTTP headers implemented

--- src/Keycloak.Net/Common/Extensions/FlurlRequestExtensions.cs
+++ src/Keycloak.Net/Common/Extensions/FlurlRequestExtensions.cs
@@ -40,5 +40,16 @@ namespace Keycloak.Net.Common.Extensions
 
             return request.WithOAuthBearerToken(GetAccessToken(url, realm, userName, password));
         }
+
+        public static IFlurlRequest WithForwardedHttpHeaders(this IFlurlRequest request, ForwardedHttpHeaders forwardedHeaders)
+        {
+            if (!string.IsNullOrEmpty(forwardedHeaders?.forwardedFor))
+                request = request.WithHeader("X-Forwarded-For", forwardedHeaders.forwardedFor);
+            if (!string.IsNullOrEmpty(forwardedHeaders?.forwardedProto))
+                request = request.WithHeader("X-Forwarded-Proto", forwardedHeaders.forwardedProto);
+            if (!string.IsNullOrEmpty(forwardedHeaders?.forwardedHost))
+                request = request.WithHeader("X-Forwarded-Host", forwardedHeaders.forwardedHost);
+            return request;
+        }
     }
 }
---
+++ src/Keycloak.Net/Common/ForwardedHttpHeaders.cs
@@ -0,0 +1,9 @@
+﻿namespace Keycloak.Net
+{
+    public class ForwardedHttpHeaders
+    {
+        public string forwardedFor { get; set; }
+        public string forwardedProto { get; set; }
+        public string forwardedHost { get; set; }
+    }
+}
