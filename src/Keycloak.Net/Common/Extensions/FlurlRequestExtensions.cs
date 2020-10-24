276c6443e471fc832c3d3139ab960963ffdb3609
Luk Vermeulen <luk.vermeulen@gmail.com>
2020-10-24 11:05:58 +0200

    fix braces

--- src/Keycloak.Net/Common/Extensions/FlurlRequestExtensions.cs
+++ src/Keycloak.Net/Common/Extensions/FlurlRequestExtensions.cs
@@ -44,11 +44,20 @@ namespace Keycloak.Net.Common.Extensions
         public static IFlurlRequest WithForwardedHttpHeaders(this IFlurlRequest request, ForwardedHttpHeaders forwardedHeaders)
         {
             if (!string.IsNullOrEmpty(forwardedHeaders?.forwardedFor))
-                request = request.WithHeader("X-Forwarded-For", forwardedHeaders.forwardedFor);
+            {
+	            request = request.WithHeader("X-Forwarded-For", forwardedHeaders.forwardedFor);
+            }
+
             if (!string.IsNullOrEmpty(forwardedHeaders?.forwardedProto))
-                request = request.WithHeader("X-Forwarded-Proto", forwardedHeaders.forwardedProto);
+            {
+	            request = request.WithHeader("X-Forwarded-Proto", forwardedHeaders.forwardedProto);
+            }
+
             if (!string.IsNullOrEmpty(forwardedHeaders?.forwardedHost))
-                request = request.WithHeader("X-Forwarded-Host", forwardedHeaders.forwardedHost);
+            {
+	            request = request.WithHeader("X-Forwarded-Host", forwardedHeaders.forwardedHost);
+            }
+
             return request;
         }
     }
