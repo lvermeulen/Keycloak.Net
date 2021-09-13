b52ceebc37e00db5e1661c7fda0c5b3950f84fee
svsk417 <svoskuhl@slashwhy.de>
2021-09-13 17:09:46 +0200

    adds doc

--- src/Keycloak.Net/Models/Clients/ClientPolicy.cs
+++ src/Keycloak.Net/Models/Clients/ClientPolicy.cs
@@ -5,6 +5,9 @@ namespace Keycloak.Net.Models.Clients
 {
     public class ClientPolicy : Policy
     {
+        /// <summary>
+        /// The ids of the clients. 
+        /// </summary>
         [JsonProperty("clients")]
         public IEnumerable<string> Clients { get; set; }
     }
